4ab677831dc0d6960ce4d6aadb246400b714aae8
Harry Whelchel <harry@convopanda.com>
2021-06-30 17:05:44 -0400

    remove unneeded example

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,43 +127,11 @@ const links = [
   },
 ]
 
-const people = [
-  {
-    name: 'Calvin Hawkins',
-    email: 'calvin.hawkins@example.com',
-    image:
-      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Kristen Ramos',
-    email: 'kristen.ramos@example.com',
-    image:
-      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Ted Fox',
-    email: 'ted.fox@example.com',
-    image:
-      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-]
-
 // markup
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
-      <ul className="divide-y divide-gray-200">
-        {people.map((person) => (
-          <li key={person.email} className="py-4 flex">
-            <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-            <div className="ml-3">
-              <p className="text-sm font-medium text-gray-900">{person.name}</p>
-              <p className="text-sm text-gray-500">{person.email}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
       <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
         <div class="flex-shrink-0">
           <img class="h-12 w-12"
