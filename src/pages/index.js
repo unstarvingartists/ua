5209599eb3bb3eec807c91f5f181e3af5903f948
Harry Whelchel <harry@convopanda.com>
2021-07-02 12:35:09 -0400

    refactor hero component to take navigation as child

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,18 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Navigation from '../components/navigation'
-import Hero from '../components/hero'
-import LogoCloud from '../components/logo-cloud'
+import Hero from '../components/home/hero'
+import LogoCloud from '../components/home/logo-cloud'
+import Problem from '../components/home/problem'
 
 export default function Home() {
   return (
     <Layout>
-      <div className="relative bg-gray-50 overflow-hidden">
-        <div className="relative pt-6 pb-16 sm:pb-24">
-          <Navigation />
-          <Hero />
-        </div>
-      </div>
+      <Hero>
+        <Navigation />
+      </Hero>
       <LogoCloud />
+      <Problem />
     </Layout>
   )
 }
