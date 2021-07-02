3577757e0df5337d9e3d5f2da440e066b20c731f
Harry Whelchel <harry@convopanda.com>
2021-07-02 14:32:44 -0400

    add skeleton design for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,14 @@ import Navigation from '../components/navigation'
 import Hero from '../components/home/hero'
 import LogoCloud from '../components/home/logo-cloud'
 import Problem from '../components/home/problem'
+import HowItWorks from '../components/home/how-it-works'
+import BattleTested from '../components/home/battle-tested'
+import WeTeach from '../components/home/we-teach'
+import CallToAction from '../components/home/call-to-action'
+import LearnTheWay from '../components/home/learn-the-way'
+import YoureInGoodCompany from '../components/home/youre-in-good-company'
+import LearnEverything from '../components/home/learn-everything'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
@@ -13,6 +21,15 @@ export default function Home() {
       </Hero>
       <LogoCloud />
       <Problem />
+      <HowItWorks />
+      <BattleTested />
+      <WeTeach />
+      <CallToAction />
+      <LearnTheWay />
+      <YoureInGoodCompany />
+      <LearnEverything />
+      <CallToAction />
+      <Footer />
     </Layout>
   )
 }
