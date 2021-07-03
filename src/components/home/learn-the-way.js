3afded218f65faecd11e9b31eff904580c8af4c1
Harry Whelchel <harry@convopanda.com>
2021-07-03 11:49:52 -0400

    update learn the way section

--- src/components/home/learn-the-way.js
+++ src/components/home/learn-the-way.js
@@ -1,51 +1,8 @@
 import * as React from "react";
-import {
-  AnnotationIcon,
-  GlobeAltIcon,
-  LightningBoltIcon,
-  MailIcon,
-  ScaleIcon,
-} from "@heroicons/react/outline";
-
-const transferFeatures = [
-  {
-    id: 1,
-    name: "Competitive exchange rates",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: GlobeAltIcon,
-  },
-  {
-    id: 2,
-    name: "No hidden fees",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: ScaleIcon,
-  },
-  {
-    id: 3,
-    name: "Transfers are instant",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: LightningBoltIcon,
-  },
-];
-const communicationFeatures = [
-  {
-    id: 1,
-    name: "Mobile notifications",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: AnnotationIcon,
-  },
-  {
-    id: 2,
-    name: "Reminder emails",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: MailIcon,
-  },
-];
+import onlineTrainingPlatform from "../../images/online-training-platform.svg";
+import interactiveArtistCommunity from "../../images/interactive-artist-community.svg";
+import expertMentorship from "../../images/expert-mentorship-on-demand.svg";
+import liveRealTime from "../../images/live-real-time-events.svg";
 
 export default function LearnTheWay() {
   return (
@@ -87,43 +44,25 @@ export default function LearnTheWay() {
 
         <div className="relative">
           <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            A better way to send money
+            Learn the way that best suits you
           </h2>
           <p className="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-            magnam voluptatum cupiditate veritatis in, accusamus quisquam.
+            Learn anytime, anywhere and whatever way you like with multiple
+            training formats. All Unstarving Artists programs come with online,
+            social, live, and simulated materials.
           </p>
         </div>
 
         <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
           <div className="relative">
             <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
-              Transfer funds world-wide
+              Online training platform
             </h3>
             <p className="mt-3 text-lg text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-              minima sequi recusandae, porro maiores officia assumenda aliquam
-              laborum ab aliquid veritatis impedit odit adipisci optio iste
-              blanditiis facere. Totam, velit.
+              Watch training videos in stunning 4K HD, listen to mp3 recordings,
+              or read text transcripts. Access the training online from any
+              desktop or mobile device, anywhere in the world, anytime you like.
             </p>
-
-            <dl className="mt-10 space-y-10">
-              {transferFeatures.map((item) => (
-                <div key={item.id} className="relative">
-                  <dt>
-                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-                      <item.icon className="h-6 w-6" aria-hidden="true" />
-                    </div>
-                    <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                      {item.name}
-                    </p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">
-                    {item.description}
-                  </dd>
-                </div>
-              ))}
-            </dl>
           </div>
 
           <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
@@ -162,8 +101,122 @@ export default function LearnTheWay() {
             <img
               className="relative mx-auto"
               width={490}
-              src="https://tailwindui.com/img/features/feature-example-1.png"
-              alt=""
+              src={onlineTrainingPlatform}
+              alt="Online Training Platform"
+            />
+          </div>
+        </div>
+
+        <div className="relative mt-12 sm:mt-16 lg:mt-24">
+          <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+            <div className="lg:col-start-2">
+              <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+                Interactive artist community
+              </h3>
+              <p className="mt-3 text-lg text-gray-500">
+                Join an energetic community of 100+ artists on the same
+                wavelength as you. Becoming a successful artist is lonely when
+                you’re surrounded by “normal people” who just don’t get it. Get
+                inspired, ask questions, generate ideas, make friends and have
+                fun!
+              </p>
+            </div>
+
+            <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
+              <svg
+                className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+                width={784}
+                height={404}
+                fill="none"
+                viewBox="0 0 784 404"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={784}
+                  height={404}
+                  fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)"
+                />
+              </svg>
+              <img
+                className="relative mx-auto"
+                width={490}
+                src={interactiveArtistCommunity}
+                alt="Interactive Artist Community"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+          <div className="relative">
+            <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+              Expert mentorship on demand
+            </h3>
+            <p className="mt-3 text-lg text-gray-500">
+              Get personalized mentorship from experts who can troubleshoot your
+              problems and provide direction. All programs include weekly Q&A
+              calls, recordings, artist interviews, livestream sessions, and
+              24/7/365 support.
+            </p>
+          </div>
+
+          <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
+            <svg
+              className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+              width={784}
+              height={404}
+              fill="none"
+              viewBox="0 0 784 404"
+            >
+              <defs>
+                <pattern
+                  id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={784}
+                height={404}
+                fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)"
+              />
+            </svg>
+            <img
+              className="relative mx-auto"
+              width={490}
+              src={expertMentorship}
+              alt="Expert Mentorship On Demand"
             />
           </div>
         </div>
@@ -206,31 +259,14 @@ export default function LearnTheWay() {
           <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
             <div className="lg:col-start-2">
               <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
-                Always in the loop
+                Live real-time events
               </h3>
               <p className="mt-3 text-lg text-gray-500">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                ex obcaecati natus eligendi delectus, cum deleniti sunt in
-                labore nihil quod quibusdam expedita nemo.
+                Attend live real-time events both online and offline. Learn in
+                an immersive “real world” environment, rub shoulders with some
+                of the most successful artists in the industry and get access to
+                bleeding edge strategy.
               </p>
-
-              <dl className="mt-10 space-y-10">
-                {communicationFeatures.map((item) => (
-                  <div key={item.id} className="relative">
-                    <dt>
-                      <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-                        <item.icon className="h-6 w-6" aria-hidden="true" />
-                      </div>
-                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                        {item.name}
-                      </p>
-                    </dt>
-                    <dd className="mt-2 ml-16 text-base text-gray-500">
-                      {item.description}
-                    </dd>
-                  </div>
-                ))}
-              </dl>
             </div>
 
             <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
@@ -270,8 +306,8 @@ export default function LearnTheWay() {
               <img
                 className="relative mx-auto"
                 width={490}
-                src="https://tailwindui.com/img/features/feature-example-2.png"
-                alt=""
+                src={liveRealTime}
+                alt="Live Real Time Events"
               />
             </div>
           </div>
