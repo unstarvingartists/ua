424b9f0cf521e43c61aa1f1eab27d857f61e89bb
Harry Whelchel <harry@convopanda.com>
2021-07-03 11:17:41 -0400

    update call to action

--- src/components/home/call-to-action.js
+++ src/components/home/call-to-action.js
@@ -6,8 +6,8 @@ export default function CallToAction() {
     <div className="bg-white">
       <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">Ready to dive in?</span>
-          <span className="block">Start your free trial today.</span>
+          <span className="block">Ready to earn more from your art?</span>
+          <span className="block">Watch our free training today.</span>
         </h2>
         <div className="mt-8 flex justify-center">
           <div className="inline-flex rounded-md shadow">
@@ -15,7 +15,7 @@ export default function CallToAction() {
               to="/opt-in"
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
             >
-              See How It Works
+              Watch Free Training
             </Link>
           </div>
           <div className="ml-3 inline-flex">
