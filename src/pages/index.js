3a52ca3ccfbe7065f88443556cf2db1f2f2617c0
Harry Whelchel <harry@convopanda.com>
2021-07-03 16:18:03 -0400

    adds methodology skeleton

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,13 @@ import Problem from "../components/home/problem";
 import HowItWorks from "../components/home/how-it-works";
 import ProvenRealWorld from "../components/home/proven-real-world";
 import WeTeach from "../components/home/we-teach";
-import CallToAction from "../components/home/call-to-action";
+import CallToAction from "../components/call-to-action";
 import LearnTheWay from "../components/home/learn-the-way";
 import YoureInGoodCompany from "../components/home/youre-in-good-company";
 import LearnEssentials from "../components/home/learn-essentials";
 import Footer from "../components/footer";
 
-export default function Home() {
+export default function Page() {
   return (
     <Layout>
       <Hero>
