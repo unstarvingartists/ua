e9c9c2d858891b36140adcd39e0b3a910d8d50c0
Harry Whelchel <harry@convopanda.com>
2021-07-03 17:28:20 -0400

    adds page scaffolding

---
+++ src/pages/book.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import Layout from "../components/layout";
+import Navigation from "../components/navigation";
+import Hero from "../components/methodology/hero";
+import CallToAction from "../components/call-to-action";
+import Footer from "../components/footer";
+
+export default function Page() {
+  return (
+    <Layout>
+      <Hero>
+        <Navigation />
+      </Hero>
+      <CallToAction />
+      <Footer />
+    </Layout>
+  );
+}
