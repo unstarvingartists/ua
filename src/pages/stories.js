fa1a8c6c765f5bae27b046129c167f8d6958459d
Harry Whelchel <harry@convopanda.com>
2021-07-04 09:30:08 -0400

    adds student stories scaffolding

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -1,7 +1,9 @@
 import * as React from "react";
 import Layout from "../components/layout";
-import Navigation from "../components/gray-navigation";
-import Hero from "../components/methodology/hero";
+import Navigation from "../components/white-navigation";
+import Hero from "../components/stories/hero";
+import LogoCloud from "../components/stories/logo-cloud";
+import StudentStories from "../components/stories/student-stories";
 import CallToAction from "../components/call-to-action";
 import Footer from "../components/footer";
 
@@ -11,6 +13,8 @@ export default function Page() {
       <Hero>
         <Navigation />
       </Hero>
+      <LogoCloud />
+      <StudentStories />
       <CallToAction />
       <Footer />
     </Layout>
