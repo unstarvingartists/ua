532bbb8f27f237366ae1c106d91b95ac89429886
Harry Whelchel <harry@convopanda.com>
2021-07-04 15:57:21 -0400

    point to clickfunnels opt in

--- src/components/call-to-action.js
+++ src/components/call-to-action.js
@@ -13,7 +13,7 @@ export default function Component() {
         <div className="mt-8 flex justify-center">
           <div className="inline-flex rounded-md shadow">
             <Link
-              to="/training"
+              to="https://go.unstarvingartists.org/opt-in"
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
             >
               See How It Works
