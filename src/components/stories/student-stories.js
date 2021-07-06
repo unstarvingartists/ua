10b36fbe87d9f6c62a214b8b7bd817c7905dbc15
Harry Whelchel <harry@convopanda.com>
2021-07-06 10:19:50 -0400

    get graphql loading working

--- src/components/stories/student-stories.js
+++ src/components/stories/student-stories.js
@@ -56,13 +56,13 @@ export default function Component() {
             <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
               {data.allMdx.edges.map((post) => (
                 <div
-                  key={post.title}
+                  key={post.node.frontmatter.title}
                   className="flex flex-col rounded-lg shadow-lg overflow-hidden"
                 >
                   <div className="flex-shrink-0">
                     <img
                       className="h-48 w-full object-cover"
-                      src={post.imageUrl}
+                      src={post.node.frontmatter.imageUrl}
                       alt=""
                     />
                   </div>
@@ -70,28 +70,33 @@ export default function Component() {
                     <div className="flex-1">
                       <p className="text-sm font-medium text-blue-600">
                         <a
-                          href={post.category.href}
+                          href={post.node.frontmatter.category.href}
                           className="hover:underline"
                         >
-                          {post.category.name}
+                          {post.node.frontmatter.category.name}
                         </a>
                       </p>
-                      <a href={post.href} className="block mt-2">
+                      <a
+                        href={post.node.frontmatter.href}
+                        className="block mt-2"
+                      >
                         <p className="text-xl font-semibold text-gray-900">
-                          {post.title}
+                          {post.node.frontmatter.title}
                         </p>
                         <p className="mt-3 text-base text-gray-500">
-                          {post.description}
+                          {post.node.frontmatter.description}
                         </p>
                       </a>
                     </div>
                     <div className="mt-6 flex items-center">
                       <div className="flex-shrink-0">
-                        <a href={post.author.href}>
-                          <span className="sr-only">{post.author.name}</span>
+                        <a href={post.node.frontmatter.author.href}>
+                          <span className="sr-only">
+                            {post.node.frontmatter.author.name}
+                          </span>
                           <img
                             className="h-10 w-10 rounded-full"
-                            src={post.author.imageUrl}
+                            src={post.node.frontmatter.author.imageUrl}
                             alt=""
                           />
                         </a>
@@ -99,16 +104,18 @@ export default function Component() {
                       <div className="ml-3">
                         <p className="text-sm font-medium text-gray-900">
                           <a
-                            href={post.author.href}
+                            href={post.node.frontmatter.author.href}
                             className="hover:underline"
                           >
-                            {post.author.name}
+                            {post.node.frontmatter.author.name}
                           </a>
                         </p>
                         <div className="flex space-x-1 text-sm text-gray-500">
-                          <time dateTime={post.datetime}>{post.date}</time>
+                          <time dateTime={post.node.frontmatter.datetime}>
+                            {post.node.frontmatter.date}
+                          </time>
                           <span aria-hidden="true">&middot;</span>
-                          <span>{post.readingTime} read</span>
+                          <span>{post.node.frontmatter.readingTime} read</span>
                         </div>
                       </div>
                     </div>
