bb26ed8f79651e61ca161d95c1fd0170d2a997c3
Harry Whelchel <harry@convopanda.com>
2021-07-14 21:13:21 -0400

    add the concert image

--- src/components/404/hero.js
+++ src/components/404/hero.js
@@ -1,35 +1,88 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import logo from "../../images/logo.svg";
 
 const Component = ({ children }) => {
   return (
-    <main
-      className="min-h-screen bg-cover bg-top sm:bg-top"
-      style={{
-        backgroundImage:
-          'url("https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75")',
-      }}
-    >
-      <div className="max-w-7xl mx-auto px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48">
-        <p className="text-sm font-semibold text-black text-opacity-50 uppercase tracking-wide">
-          404 error
-        </p>
-        <h1 className="mt-2 text-4xl font-extrabold text-white tracking-tight sm:text-5xl">
-          Uh oh! I think you’re lost.
-        </h1>
-        <p className="mt-2 text-lg font-medium text-black text-opacity-50">
-          It looks like the page you’re looking for doesn't exist.
-        </p>
-        <div className="mt-6">
-          <Link
-            to="/"
-            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black text-opacity-75 bg-white bg-opacity-75 sm:bg-opacity-25 sm:hover:bg-opacity-50"
-          >
-            Go back home
-          </Link>
-        </div>
+    <div className="bg-white min-h-screen flex flex-col lg:relative">
+      <div className="flex-grow flex flex-col">
+        <main className="flex-grow flex flex-col bg-white">
+          <div className="flex-grow mx-auto max-w-7xl w-full flex flex-col px-4 sm:px-6 lg:px-8">
+            <div className="flex-shrink-0 pt-10 sm:pt-16">
+              <Link to="/" className="inline-flex">
+                <span className="sr-only">Workflow</span>
+                <img
+                  className="h-12 w-auto"
+                  src={logo}
+                  alt="Unstarving Artists Logo"
+                />
+              </Link>
+            </div>
+            <div className="flex-shrink-0 my-auto py-16 sm:py-32">
+              <p className="text-sm font-semibold text-blue-600 uppercase tracking-wide">
+                404 error
+              </p>
+              <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                Page not found
+              </h1>
+              <p className="mt-2 text-base text-gray-500">
+                Sorry, we couldn’t find the page you’re looking for.
+              </p>
+              <div className="mt-6">
+                <Link
+                  to="/"
+                  className="text-base font-medium text-blue-600 hover:text-blue-500"
+                >
+                  Go back home<span aria-hidden="true"> &rarr;</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </main>
+        <footer className="flex-shrink-0 bg-gray-50">
+          <div className="mx-auto max-w-7xl w-full px-4 py-16 sm:px-6 lg:px-8">
+            <nav className="flex space-x-4">
+              <Link
+                to="/methodology"
+                className="text-sm font-medium text-gray-500 hover:text-gray-600"
+              >
+                Our Methodology
+              </Link>
+              <span
+                className="inline-block border-l border-gray-300"
+                aria-hidden="true"
+              />
+              <Link
+                to="/stories"
+                className="text-sm font-medium text-gray-500 hover:text-gray-600"
+              >
+                Student Stories
+              </Link>
+              <span
+                className="inline-block border-l border-gray-300"
+                aria-hidden="true"
+              />
+              <Link
+                to="/faq"
+                className="text-sm font-medium text-gray-500 hover:text-gray-600"
+              >
+                FAQ
+              </Link>
+            </nav>
+          </div>
+        </footer>
       </div>
-    </main>
+      <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <StaticImage
+          className="absolute inset-0 h-full w-full object-cover"
+          src="../../images/the-concert.jpeg"
+          width={1280}
+          height={1440}
+          alt="The Concert by Vermeer"
+        />
+      </div>
+    </div>
   );
 };
 export default Component;
