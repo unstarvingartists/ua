01e349990e909c31aba9fb22dd8e8ae0d3443553
Dmitriy <dimidrol131211@gmail.com>
2021-11-25 10:20:11 +0200

    update page paths

--- src/pages/book.js
+++ src/pages/book.js
@@ -1,16 +1,19 @@
-import * as React from "react";
+import React from "react";
 import Layout from "../components/layout";
-import Navigation from "../components/training-navigation";
-import Hero from "../components/methodology/hero";
+import Navigation from "../components/funnel/navigation";
 import Seo from "../components/seo";
+import { InlineWidget } from "react-calendly";
 
-export default function Page({ location }) {
+export default function BookControl({ location }) {
   return (
-    <Layout>
-      <Seo title="Free Training" pathname={location.pathname} />
-      <Hero>
-        <Navigation />
-      </Hero>
-    </Layout>
+    <>
+      <Layout>
+        <Seo title="Book a Strategy Session" pathname={location.pathname} />
+        <div className="pt-10">
+          <Navigation />
+        </div>
+        <InlineWidget url="https://calendly.com/unstarvingartists/artist?hide_gdpr_banner=1&text_color=212529&primary_color=007bff" />
+      </Layout>
+    </>
   );
 }
