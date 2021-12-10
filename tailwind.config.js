17d31ef8c2be5360795b1b3fb4455b4f41992cdd
Harry Whelchel <harry@convopanda.com>
2021-12-10 00:47:12 -0500

    wip tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
