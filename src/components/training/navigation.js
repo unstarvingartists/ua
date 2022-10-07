d7008fde73588354960634d2a3a519446a190b2e
Harry Whelchel <harry@convopanda.com>
2022-10-07 19:37:02 -0400

    remove sc funnel pages

--- src/components/training/navigation.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Navigation() {
-  return (
-    <div className="max-w-6xl px-4 mx-auto sm:px-6">
-      <p className="text-[1.1875rem] text- font-semibold">Unstarving Artists</p>
-    </div>
-  );
-}
