4fb5ba946b22dc436048675061a828f82cafcb54
Harry Whelchel <harry@convopanda.com>
2022-10-09 17:31:07 -0400

    wip

--- src/components/community/hero.js
+++ src/components/community/hero.js
@@ -11,36 +11,36 @@ export default function Component({ children }) {
           <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
               <span className="block xl:inline">
-                Join Our FREE Facebook Group For
+                Join Our FREE Community For
               </span>{" "}
               <span className="block text-blue-600 xl:inline">
                 Artists Today!
               </span>
             </h1>
             <p className="font-bold mt-3 max-w-md mx-auto text-lg text-gray-900 sm:text-xl md:mt-5 md:max-w-3xl">
-              Inside, You Will Get Access To...
+              Follow Harry On Instagram And Get Access To...
             </p>
             <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
               ⚡ Exclusive training on the process we used to earn a 5 figure a
-              month income from our art (Our FREE gift to you for joining).
+              month income from art (Our FREE gift to you for following).
             </p>
             <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
               ⚡ Real-life case studies from our best clients showing you
               exactly what's working right now.
             </p>
             <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-              ⚡ Regular trainings & interviews with industry leading experts
-              (plus special insider access for paid members).
+              ⚡ Regular art income tips & tricks from Harry and the team as
+              well as industry leading experts.
             </p>
             <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
               <div className="rounded-md shadow">
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://www.facebook.com/groups/unstarvingartistscommunity"
+                  href="https://instagram.com/harrywhelchel/"
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
                 >
-                  Join Facebook Group Now
+                  Follow On Instagram Now
                 </a>
               </div>
             </div>
