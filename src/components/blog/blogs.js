93bf01e7e18fee4fd1f5c08451c8c713fd2c9cfc
Harry Whelchel <harry@convopanda.com>
2022-12-30 21:02:45 -0500

    fix deprecation error

--- src/components/blog/blogs.js
+++ src/components/blog/blogs.js
@@ -1,131 +1,127 @@
 import * as React from "react";
-import { StaticQuery, graphql, Link } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 export default function Component() {
-  return (
-    <StaticQuery
-      query={graphql`
-        query blogsQuery {
-          allMdx(
-            filter: { frontmatter: { category: { name: { eq: "Blog Post" } } } }
-            sort: { frontmatter: { id: DESC } }
-          ) {
-            edges {
-              node {
-                id
-                frontmatter {
-                  category {
-                    href
-                    name
-                    label
+  const data = useStaticQuery(graphql`
+    query blogsQuery {
+      allMdx(
+        filter: { frontmatter: { category: { name: { eq: "Blog Post" } } } }
+        sort: { frontmatter: { id: DESC } }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              category {
+                href
+                name
+                label
+              }
+              description
+              href
+              title
+              author {
+                href
+                name
+                location
+                title
+                image {
+                  childImageSharp {
+                    gatsbyImageData(width: 40)
                   }
-                  description
-                  href
-                  title
-                  author {
-                    href
-                    name
-                    location
-                    title
-                    image {
-                      childImageSharp {
-                        gatsbyImageData(width: 40)
-                      }
-                    }
-                  }
-                  image {
-                    childImageSharp {
-                      gatsbyImageData(width: 468)
-                    }
-                  }
+                }
+              }
+              image {
+                childImageSharp {
+                  gatsbyImageData(width: 468)
                 }
               }
             }
           }
         }
-      `}
-      render={(data) => (
-        <div className="relative bg-gray-50 pt-4 pb-20 px-4 sm:px-6 lg:pt-8 lg:pb-28 lg:px-8">
-          <div className="absolute inset-0">
-            <div className="bg-white h-1/3 sm:h-2/3" />
-          </div>
-          <div className="relative max-w-7xl mx-auto">
-            <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-              {data.allMdx.edges.map(({ node: post }) => {
-                const image = getImage(post.frontmatter.image);
-                const authorImage = getImage(post.frontmatter.author.image);
+      }
+    }
+  `);
+  return (
+    <div className="relative bg-gray-50 pt-4 pb-20 px-4 sm:px-6 lg:pt-8 lg:pb-28 lg:px-8">
+      <div className="absolute inset-0">
+        <div className="bg-white h-1/3 sm:h-2/3" />
+      </div>
+      <div className="relative max-w-7xl mx-auto">
+        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+          {data.allMdx.edges.map(({ node: post }) => {
+            const image = getImage(post.frontmatter.image);
+            const authorImage = getImage(post.frontmatter.author.image);
 
-                return (
-                  <div
-                    key={post.id}
-                    className="flex flex-col rounded-lg shadow-lg overflow-hidden"
-                  >
+            return (
+              <div
+                key={post.id}
+                className="flex flex-col rounded-lg shadow-lg overflow-hidden"
+              >
+                <div className="flex-shrink-0">
+                  <Link to={post.frontmatter.href}>
+                    <GatsbyImage
+                      className="h-48 w-full object-cover"
+                      image={image}
+                      alt={post.frontmatter.title}
+                    />
+                  </Link>
+                </div>
+                <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                  <div className="flex-1">
+                    <p className="text-sm font-medium text-blue-600">
+                      <Link
+                        to={post.frontmatter.href}
+                        className="hover:underline"
+                      >
+                        {post.frontmatter.category.label}
+                      </Link>
+                    </p>
+                    <Link to={post.frontmatter.href} className="block mt-2">
+                      <p className="text-xl font-semibold text-gray-900">
+                        {post.frontmatter.title}
+                      </p>
+                      <p className="mt-3 text-base text-gray-500">
+                        {post.frontmatter.description}
+                      </p>
+                    </Link>
+                  </div>
+                  <div className="mt-6 flex items-center">
                     <div className="flex-shrink-0">
-                      <Link to={post.frontmatter.href}>
+                      <Link to={post.frontmatter.author.href}>
+                        <span className="sr-only">
+                          {post.frontmatter.author.name}
+                        </span>
                         <GatsbyImage
-                          className="h-48 w-full object-cover"
-                          image={image}
-                          alt={post.frontmatter.title}
+                          className="h-10 w-10 rounded-full"
+                          image={authorImage}
+                          alt={post.frontmatter.author.name}
                         />
                       </Link>
                     </div>
-                    <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                      <div className="flex-1">
-                        <p className="text-sm font-medium text-blue-600">
-                          <Link
-                            to={post.frontmatter.href}
-                            className="hover:underline"
-                          >
-                            {post.frontmatter.category.label}
-                          </Link>
-                        </p>
-                        <Link to={post.frontmatter.href} className="block mt-2">
-                          <p className="text-xl font-semibold text-gray-900">
-                            {post.frontmatter.title}
-                          </p>
-                          <p className="mt-3 text-base text-gray-500">
-                            {post.frontmatter.description}
-                          </p>
+                    <div className="ml-3">
+                      <p className="text-sm font-medium text-gray-900">
+                        <Link
+                          to={post.frontmatter.author.href}
+                          className="hover:underline"
+                        >
+                          {post.frontmatter.author.name}
                         </Link>
-                      </div>
-                      <div className="mt-6 flex items-center">
-                        <div className="flex-shrink-0">
-                          <Link to={post.frontmatter.author.href}>
-                            <span className="sr-only">
-                              {post.frontmatter.author.name}
-                            </span>
-                            <GatsbyImage
-                              className="h-10 w-10 rounded-full"
-                              image={authorImage}
-                              alt={post.frontmatter.author.name}
-                            />
-                          </Link>
-                        </div>
-                        <div className="ml-3">
-                          <p className="text-sm font-medium text-gray-900">
-                            <Link
-                              to={post.frontmatter.author.href}
-                              className="hover:underline"
-                            >
-                              {post.frontmatter.author.name}
-                            </Link>
-                          </p>
-                          <div className="flex space-x-1 text-sm text-gray-500">
-                            <span>{post.frontmatter.author.title}</span>
-                            <span aria-hidden="true">&middot;</span>
-                            <span>{post.frontmatter.author.location}</span>
-                          </div>
-                        </div>
+                      </p>
+                      <div className="flex space-x-1 text-sm text-gray-500">
+                        <span>{post.frontmatter.author.title}</span>
+                        <span aria-hidden="true">&middot;</span>
+                        <span>{post.frontmatter.author.location}</span>
                       </div>
                     </div>
                   </div>
-                );
-              })}
-            </div>
-          </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
-      )}
-    />
+      </div>
+    </div>
   );
 }
--- src/components/home/youre-in-good-company.js
+++ src/components/home/youre-in-good-company.js
@@ -1,179 +1,172 @@
 import * as React from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import { Link, StaticQuery, graphql } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
 export default function Component() {
-  return (
-    <StaticQuery
-      query={graphql`
-        query inGoodCompanyQuery {
-          allMdx(
-            filter: {
-              frontmatter: { category: { name: { eq: "Student Review" } } }
-            }
-            sort: { frontmatter: { id: DESC } }
-            limit: 3
-          ) {
-            edges {
-              node {
-                id
-                frontmatter {
-                  category {
-                    href
-                    name
+  const data = useStaticQuery(graphql`
+    query inGoodCompanyQuery {
+      allMdx(
+        filter: {
+          frontmatter: { category: { name: { eq: "Student Review" } } }
+        }
+        sort: { frontmatter: { id: DESC } }
+        limit: 3
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              category {
+                href
+                name
+              }
+              country
+              description
+              genre
+              href
+              student {
+                href
+                name
+                image {
+                  childImageSharp {
+                    gatsbyImageData(width: 40)
                   }
-                  country
-                  description
-                  genre
-                  href
-                  student {
-                    href
-                    name
-                    image {
-                      childImageSharp {
-                        gatsbyImageData(width: 40)
-                      }
-                    }
-                  }
-                  title
-                  image {
-                    childImageSharp {
-                      gatsbyImageData(width: 468)
-                    }
-                  }
+                }
+              }
+              title
+              image {
+                childImageSharp {
+                  gatsbyImageData(width: 468)
                 }
               }
             }
           }
         }
-      `}
-      render={(data) => (
-        <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-          <div className="absolute inset-0">
-            <div className="bg-white h-1/3 sm:h-2/3" />
-          </div>
-          <div className="relative max-w-7xl mx-auto">
-            <div className="text-center">
-              <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-                You're in good company
-              </h2>
-              <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-                Hundreds of artists around the world have used Unstarving
-                Artists to start and grow their art practices, expand their
-                minds, and change their lives.
-              </p>
-            </div>
-            <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-              {data.allMdx.edges.map(({ node: post }) => {
-                const image = getImage(post.frontmatter.image);
-                const studentImage = getImage(post.frontmatter.student.image);
-                return (
-                  <div
-                    key={post.id}
-                    className="flex flex-col rounded-lg shadow-lg overflow-hidden"
-                  >
+      }
+    }
+  `);
+  return (
+    <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div className="absolute inset-0">
+        <div className="bg-white h-1/3 sm:h-2/3" />
+      </div>
+      <div className="relative max-w-7xl mx-auto">
+        <div className="text-center">
+          <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+            You're in good company
+          </h2>
+          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+            Hundreds of artists around the world have used Unstarving Artists to
+            start and grow their art practices, expand their minds, and change
+            their lives.
+          </p>
+        </div>
+        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+          {data.allMdx.edges.map(({ node: post }) => {
+            const image = getImage(post.frontmatter.image);
+            const studentImage = getImage(post.frontmatter.student.image);
+            return (
+              <div
+                key={post.id}
+                className="flex flex-col rounded-lg shadow-lg overflow-hidden"
+              >
+                <div className="flex-shrink-0">
+                  <Link to={post.frontmatter.student.href}>
+                    <GatsbyImage
+                      className="h-48 w-full object-cover"
+                      image={image}
+                      alt={post.frontmatter.title}
+                    />
+                  </Link>
+                </div>
+                <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                  <div className="flex-1">
+                    <p className="text-sm font-medium text-blue-600">
+                      <Link
+                        to={post.frontmatter.student.href}
+                        className="hover:underline"
+                      >
+                        {post.frontmatter.category.name}
+                      </Link>
+                    </p>
+                    <Link to={post.frontmatter.href} className="block mt-2">
+                      <p className="text-xl font-semibold text-gray-900">
+                        {post.frontmatter.title}
+                      </p>
+                      <p className="mt-3 text-base text-gray-500">
+                        {post.frontmatter.description}
+                      </p>
+                    </Link>
+                  </div>
+                  <div className="mt-6 flex items-center">
                     <div className="flex-shrink-0">
                       <Link to={post.frontmatter.student.href}>
+                        <span className="sr-only">
+                          {post.frontmatter.student.name}
+                        </span>
                         <GatsbyImage
-                          className="h-48 w-full object-cover"
-                          image={image}
-                          alt={post.frontmatter.title}
+                          className="h-10 w-10 rounded-full"
+                          image={studentImage}
+                          alt={post.frontmatter.student.name}
                         />
                       </Link>
                     </div>
-                    <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                      <div className="flex-1">
-                        <p className="text-sm font-medium text-blue-600">
-                          <Link
-                            to={post.frontmatter.student.href}
-                            className="hover:underline"
-                          >
-                            {post.frontmatter.category.name}
-                          </Link>
-                        </p>
-                        <Link to={post.frontmatter.href} className="block mt-2">
-                          <p className="text-xl font-semibold text-gray-900">
-                            {post.frontmatter.title}
-                          </p>
-                          <p className="mt-3 text-base text-gray-500">
-                            {post.frontmatter.description}
-                          </p>
+                    <div className="ml-3">
+                      <p className="text-sm font-medium text-gray-900">
+                        <Link
+                          to={post.frontmatter.student.href}
+                          className="hover:underline"
+                        >
+                          {post.frontmatter.student.name}
                         </Link>
-                      </div>
-                      <div className="mt-6 flex items-center">
-                        <div className="flex-shrink-0">
-                          <Link to={post.frontmatter.student.href}>
-                            <span className="sr-only">
-                              {post.frontmatter.student.name}
-                            </span>
-                            <GatsbyImage
-                              className="h-10 w-10 rounded-full"
-                              image={studentImage}
-                              alt={post.frontmatter.student.name}
-                            />
-                          </Link>
-                        </div>
-                        <div className="ml-3">
-                          <p className="text-sm font-medium text-gray-900">
-                            <Link
-                              to={post.frontmatter.student.href}
-                              className="hover:underline"
-                            >
-                              {post.frontmatter.student.name}
-                            </Link>
-                          </p>
-                          <div className="flex space-x-1 text-sm text-gray-500">
-                            <span>{post.frontmatter.genre}</span>
-                            <span aria-hidden="true">&middot;</span>
-                            <span>{post.frontmatter.country}</span>
-                          </div>
-                        </div>
+                      </p>
+                      <div className="flex space-x-1 text-sm text-gray-500">
+                        <span>{post.frontmatter.genre}</span>
+                        <span aria-hidden="true">&middot;</span>
+                        <span>{post.frontmatter.country}</span>
                       </div>
                     </div>
                   </div>
-                );
-              })}
-            </div>
-            <div className="mt-10 text-center">
-              <div className="mt-6 mb-1">
-                <div className="inline-flex items-center">
-                  <div className="flex-shrink-0 flex">
-                    <StarIcon
-                      className="h-10 w-10 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                    <StarIcon
-                      className="h-10 w-10 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                    <StarIcon
-                      className="h-10 w-10 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                    <StarIcon
-                      className="h-10 w-10 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                    <StarIcon
-                      className="h-10 w-10 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                  </div>
                 </div>
               </div>
-              <Link
-                to="/reviews"
-                className="text-base font-medium text-blue-600"
-              >
-                {" "}
-                4.9 stars from 45 student reviews{" "}
-                <span aria-hidden="true">&rarr;</span>{" "}
-              </Link>
+            );
+          })}
+        </div>
+        <div className="mt-10 text-center">
+          <div className="mt-6 mb-1">
+            <div className="inline-flex items-center">
+              <div className="flex-shrink-0 flex">
+                <StarIcon
+                  className="h-10 w-10 text-yellow-400"
+                  aria-hidden="true"
+                />
+                <StarIcon
+                  className="h-10 w-10 text-yellow-400"
+                  aria-hidden="true"
+                />
+                <StarIcon
+                  className="h-10 w-10 text-yellow-400"
+                  aria-hidden="true"
+                />
+                <StarIcon
+                  className="h-10 w-10 text-yellow-400"
+                  aria-hidden="true"
+                />
+                <StarIcon
+                  className="h-10 w-10 text-yellow-400"
+                  aria-hidden="true"
+                />
+              </div>
             </div>
           </div>
+          <Link to="/reviews" className="text-base font-medium text-blue-600">
+            {" "}
+            4.9 stars from 45 student reviews{" "}
+            <span aria-hidden="true">&rarr;</span>{" "}
+          </Link>
         </div>
-      )}
-    />
+      </div>
+    </div>
   );
 }
